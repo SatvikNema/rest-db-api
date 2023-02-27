c976feb4557aca30d5ca04e0a002fe753162bc55
Satvik <satvik.nema@walmart.com>
2023-02-27 16:23:57 +0530

    add pyproject.toml

--- examples/testDialect.py
+++ examples/testDialect.py
@@ -27,36 +27,36 @@ from restDbApi.utils import get_virtual_table
 #     print(i)
 
 
-engine = create_engine("rest://stg.wsp-store-info.walmart.com?ishttps=0")
-connection = engine.connect()
-
-endpoint = '/wsp-store-info/v1/layers/pipelinestores'
-
-params = {
-    'hlong': '-98.04269584580887',
-    'llat': '30.660521215372583',
-    'hlat': '34.51010643175928',
-    'llong': '-117.87603469278673',
-    'pipelinestores': 'APPROVED_LX,IDEA_LX,PROJECTS_LX'
-}
-
-headers = {
-    'Content-Type': 'application/json',
-    'WM_CONSUMER.ID': '7aa13f7b-5e29-451b-bcfa-ba1b62471b89',
-    'WM_SVC.ENV': 'stg',
-    'WM_SVC.NAME': 'WSP-STORE-INFO',
-    'sessionId': 'czBuMDJxbQ==',
-}
-
-jsonpath = "$[*]"
-
-virtual_table = get_virtual_table(endpoint=endpoint,
-                                  params=params,
-                                  headers=headers,
-                                  jsonpath=jsonpath)
-print(virtual_table)
-for i in connection.execute(f'SELECT * FROM "{virtual_table}"'):
-    print(i)
+# engine = create_engine("rest://stg.wsp-store-info.walmart.com?ishttps=0")
+# connection = engine.connect()
+#
+# endpoint = '/wsp-store-info/v1/layers/pipelinestores'
+#
+# params = {
+#     'hlong': '-98.04269584580887',
+#     'llat': '30.660521215372583',
+#     'hlat': '34.51010643175928',
+#     'llong': '-117.87603469278673',
+#     'pipelinestores': 'APPROVED_LX,IDEA_LX,PROJECTS_LX'
+# }
+#
+# headers = {
+#     'Content-Type': 'application/json',
+#     'WM_CONSUMER.ID': '7aa13f7b-5e29-451b-bcfa-ba1b62471b89',
+#     'WM_SVC.ENV': 'stg',
+#     'WM_SVC.NAME': 'WSP-STORE-INFO',
+#     'sessionId': 'czBuMDJxbQ==',
+# }
+#
+# jsonpath = "$[*]"
+#
+# virtual_table = get_virtual_table(endpoint=endpoint,
+#                                   params=params,
+#                                   headers=headers,
+#                                   jsonpath=jsonpath)
+# print(virtual_table)
+# for i in connection.execute(f'SELECT * FROM "{virtual_table}"'):
+#     print(i)
 
 
 
